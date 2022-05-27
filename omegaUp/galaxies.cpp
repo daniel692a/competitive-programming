c4f2886111a5978afff72ad6a08e81067850bb36
daniel692a <daniel62armas@gmail.com>
2022-05-26 22:59:21 -0500

    Solve Galaxies problem

---
+++ omegaUp/galaxies.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int a, b, c;
+    cin>>a>>b>>c;
+    cout<<((a<b)?"True ":"False ");
+    cout<<((c>a)?"True ":"False ");
+    cout<<((a==b)?"True ":"False ");
+    cout<<((a!=c)?"True ":"False ");
+    cout<<((c<=b)?"True":"False");
+    return 0;
+}
