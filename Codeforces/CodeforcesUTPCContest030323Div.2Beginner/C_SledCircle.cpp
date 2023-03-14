66cfefd660d2f21db912cfa5d4796a2121b5a9fc
Daniel Armas Ramírez <daniel62armas@gmail.com>
2023-03-14 01:18:23 -0600

    Solve problems at HackerRank

--- Codeforces/CodeforcesUTPCContest030323Div.2Beginner/C_SledCircle.cpp
+++ Codeforces/CodeforcesUTPCContest030323Div.2Beginner/C_SledCircle.cpp
@@ -2,6 +2,7 @@
 // problem-url: https://codeforces.com/gym/104217/problem/C
 #include <bits/stdc++.h>
 #include <ios>
+#include <unordered_set>
 #include <vector>
 
 using namespace std;
@@ -10,8 +11,30 @@ void solve(){
     int n;
     cin>>n;
     vector<int> dogPosition(n);
-    for(int &dog: dogPosition){
-        cin>>dog;
+    vector<int> distance(n);
+    int t=0;
+    for(int i=0; i<n; i++){
+        cin>>distance[i];
+        dogPosition[i] = i;
+    }
+    int p=-1;
+    unordered_set<int> pos;
+    while(t!=1002){
+        for (int i=0; i<n; i++) {
+            dogPosition[i] = (dogPosition[i]+distance[i])%n;
+            pos.insert(dogPosition[i]);
+        }
+        t++;
+        if(pos.size()==1){
+            p = dogPosition[0];
+            break;
+        }
+        pos.clear();
+    }
+    if(p!=-1){
+        cout<<t<<" "<<p<<"\n";
+    } else {
+        cout<<-1<<" "<<-1<<"\n";
     }
 }
 
---
+++ HackerRank/Handshake.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+//O(1)
+int handshake(int n) {
+    return ((n-1)*(n))/2;
+}
